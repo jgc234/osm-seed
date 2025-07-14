bc3eb7c87710292c8de90f44b9e5ef5986e43ca4
Jim Crumpler <jim@crumpler.id.au>
2025-07-14 18:54:49 +1000

    update

--- images/populate-apidb/start.sh
+++ images/populate-apidb/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 export VOLUME_DIR=/mnt/data
-export PGPASSWORD=$POSTGRES_PASSWORD
+export PGPASSWORD="${POSTGRES_PASSWORD}"
 
 # OSMOSIS tuning: https://wiki.openstreetmap.org/wiki/Osmosis/Tuning,https://lists.openstreetmap.org/pipermail/talk/2012-October/064771.html
 if [ -z "$MEMORY_JAVACMD_OPTIONS" ]; then
@@ -18,33 +18,33 @@ osmFile=$VOLUME_DIR/$file
 
 function importData () {
     # This is using a osmosis 0.47. TODO: test with osmosis 0.48, and remove the following line
-    psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "ALTER TABLE users ADD COLUMN nearby VARCHAR;"
+    # psql -U ${POSTGRES_USER} -h ${POSTGRES_HOST} -d ${POSTGRES_DB} -c "ALTER TABLE users ADD COLUMN nearby VARCHAR;"
     # In case the import file is a PBF
     if [ ${osmFile: -4} == ".pbf" ]; then
-        pbfFile=$osmFile
+        pbfFile=${osmFile}
         echo "Importing $pbfFile ..."
         osmosis --read-pbf \
-        file=$pbfFile\
+        file=${pbfFile} \
         --write-apidb \
-        host=$POSTGRES_HOST \
-        database=$POSTGRES_DB \
-        user=$POSTGRES_USER \
-        password=$POSTGRES_PASSWORD \
+        host=${POSTGRES_HOST} \
+        database=${POSTGRES_DB} \
+        user=${POSTGRES_USER} \
+        password="${POSTGRES_PASSWORD}" \
         allowIncorrectSchemaVersion=yes \
         validateSchemaVersion=no
     else
         # In case the file is .osm
         # Extract the osm file
-        bzip2 -d $osmFile
+        bzip2 -d ${osmFile}
         osmFile=${osmFile%.*}
         echo "Importing $osmFile ..."
         osmosis --read-xml \
-        file=$osmFile  \
+        file=${osmFile} \
         --write-apidb \
-        host=$POSTGRES_HOST \
-        database=$POSTGRES_DB \
-        user=$POSTGRES_USER \
-        password=$POSTGRES_PASSWORD \
+        host=${POSTGRES_HOST} \
+        database=${POSTGRES_DB} \
+        user=${POSTGRES_USER} \
+        password="${POSTGRES_PASSWORD}" \
         validateSchemaVersion=no
     fi
     # Run required fixes in DB
@@ -58,7 +58,7 @@ function importData () {
 
 flag=true
 while "$flag" = true; do
-    pg_isready -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER >/dev/null 2>&2 || continue
+    pg_isready -h ${POSTGRES_HOST} -p 5432 -U ${POSTGRES_USER} >/dev/null 2>&2 || continue
     # Change flag to false to stop ping the DB
     flag=false
     importData
